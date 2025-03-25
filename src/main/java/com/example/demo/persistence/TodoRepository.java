87ffcfd0c529a15aed29d847bbdca5ade411dfa3
shchoi8347 <csh@duksung.ac.kr>
2025-03-25 11:44:52 +0900

    수업완료코드: 02반 20250325(화)

--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -12,8 +12,9 @@ public interface TodoRepository extends JpaRepository<TodoEntity, String> {
     // save, findById, delete( ) 기본메소드가 자동으로 구현됨
 
     // 새로운 메소드 추가
-    @Query(value="select * from Todo t where t.userId = ?1")
+    //@Query(value="select * from Todo t where t.userId = ?1")
     List<TodoEntity> findByUserId(String userId);
+
     List<TodoEntity> findByUserIdAndTitle(String userId, String title);
     List<TodoEntity> findByTitle(String title);
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -3,9 +3,14 @@ package com.example.demo.service;
 
 import com.example.demo.domain.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
 @Service
 public class TodoService {
 
@@ -21,4 +26,60 @@ public class TodoService {
 
         return savedEntity.getTitle();
     }
+
+    public List<TodoEntity> create(TodoEntity entity) {
+        validate(entity);
+
+        repository.save(entity);
+
+        log.info("entity id: {} is created", entity.getId());
+
+        return repository.findByUserId(entity.getUserId() );
+    }
+
+    private void validate(TodoEntity entity) {
+        if(entity == null) {
+            log.warn("entity is null");
+            throw new RuntimeException("entity is null");
+        }
+
+        if(entity.getUserId() == null) {
+            log.warn("entity.getUserId is null");
+            throw new RuntimeException("entity.getUserId is null");
+        }
+    }
+
+    public List<TodoEntity> retrieve(final String userId) {
+        return repository.findByUserId(userId);
+    }
+
+    public List<TodoEntity> update(final TodoEntity entity) {
+        validate(entity);
+
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+        original.ifPresent(todo -> {
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+
+            repository.save(todo);
+        });
+
+        return retrieve(entity.getUserId());
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
